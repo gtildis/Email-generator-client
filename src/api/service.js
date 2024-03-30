9de960a067d9bcc82ddd74aa57b8a8f6a181535f
gtildis <gretil_15@hotmail.com>
2024-03-30 13:49:36 +0100

    initial commit and add service for companies

---
+++ src/api/service.js
@@ -0,0 +1,40 @@
+// src/api/service.js
+
+import axios from "axios";
+
+const api = axios.create({
+	// make sure you use PORT = 5005 (the port where our server is running)
+	baseURL: "http://localhost:5005/api",
+	// withCredentials: true // => you might need this option if using cookies and sessions
+});
+
+const errorHandler = (err) => {
+	throw err;
+};
+
+const getCompanies = () => {
+	return api
+		.get("/")
+		.then((res) => res.data)
+		.catch(errorHandler);
+};
+
+const uploadImage = (file) => {
+	return api
+		.post("/upload", file)
+		.then((res) => res.data)
+		.catch(errorHandler);
+};
+
+const createCompany = (newCompany) => {
+	return api
+		.post("/companies", newCompany)
+		.then((res) => res.data)
+		.catch(errorHandler);
+};
+
+export default {
+	getCompanies,
+	uploadImage,
+	createCompany,
+};
